3eeaab2e9610a058763935cd472235e1eec561bf
Bacon <iam@baconi.ng>
2024-06-11 17:14:52 -0700

    add view hierarchy

---
+++ HQTV-Patch/HQTV-Patch/HQTV_Patch.h
@@ -0,0 +1,16 @@
+//
+//  HQTV_Patch.h
+//  HQTV-Patch
+//
+//  Created by Bacon on 6/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HQTV_Patch.
+FOUNDATION_EXPORT double HQTV_PatchVersionNumber;
+
+//! Project version string for HQTV_Patch.
+FOUNDATION_EXPORT const unsigned char HQTV_PatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HQTV_Patch/PublicHeader.h>
---
+++ HQTV-Patch/HQTV-Patch/HQTV_Patch.m
@@ -0,0 +1,16 @@
+//
+//  HQTV_Patch.m
+//  HQTV-Patch
+//
+//  Created by Bacon on 6/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation HQTV_Patch : NSObject 
+
++ (void)load {
+    NSLog(@"[HQTV] WOAHAHHAHAHAHHA");
+}
+
+@end
