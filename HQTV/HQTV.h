0cb217790261ba7a00d901e8bf6ab0033cbd0707
Bacon <iam@baconi.ng>
2024-06-16 15:44:23 -0400

    cont: f6f447b
    it failed to add these files for whatever reason

---
+++ HQTV/HQTV.h
@@ -0,0 +1,20 @@
+//
+//  HQTV.h
+//  HQTV
+//
+//  Created by Bacon on 6/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HQTV
+FOUNDATION_EXPORT double HQTVVersionNumber;
+
+//! Project version string for HQTV
+FOUNDATION_EXPORT const unsigned char HQTVVersionString[];
+
+@interface HQTV : NSObject
+
++ (void)load;
+
+@end
